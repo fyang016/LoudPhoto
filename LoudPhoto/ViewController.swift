7e4089e0432611c1e12a00a728f4bf2a7d733695
Franklin Yang <fyang016@ucr.edu>
2020-10-19 17:20:06 -0700

    Initial Commit

---
+++ LoudPhoto/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LoudPhoto
+//
+//  Created by AdrenResi on 10/19/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
